f03fe085f293eafcdc1070697e027e422d332638
IVKrylova <ivk.lis@yandex.ru>
2022-05-24 22:20:06 +0300

    feat: create logger

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+// подключаем логгер запросов
+app.use(requestLogger);
+
 // все роуты приложения
 app.use(routes);
+
+// подключаем логгер ошибок
+app.use(errorLogger);
 
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/moviesdb');
